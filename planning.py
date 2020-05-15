eb87e18d1b38f5311e7182332256647479564452
j.gomez.de.la.cueva <j.gomez.de.la.cueva@accenture.com>
2020-05-15 19:01:19 +0200

    Mejora en planning
    	* Genera una nueva pestaña con el planning por responsable

--- planning.py
+++ planning.py
@@ -9,7 +9,8 @@ import pandas as pd
 import os
 from pathlib import Path
 from xlsxwriter.exceptions import FileCreateError
-
+from collections import defaultdict
+import datetime
 
 from incurridos.dao import IncurridosDAOExcel 
 from gestion.dao import GestionDAOExcel
@@ -17,49 +18,7 @@ from config import config
 from tarea.dao import TareaDAOExcel
 from tarea.view import TareaView, TareasSinPlanificarView, TareasPlanning
 from tarea.model import Tarea
-
-# def fixed_map(option):
-#     # Fix for setting text colour for Tkinter 8.6.9
-#     # From: https://core.tcl.tk/tk/info/509cafafae
-#     #
-#     # Returns the style map for 'option' with any styles starting with
-#     # ('!disabled', '!selected', ...) filtered out.
-#  
-#     # style.map() returns an empty list for missing options, so this
-#     # should be future-safe.
-#     return [elm for elm in style.map('Treeview', query_opt=option) if
-#       elm[:2] != ('!disabled', '!selected')]
-
-
-
-
-# class Arbol(ttk.Frame):
-#     def __init__(self,main_window):
-#         super().__init__(main_window)
-#         main_window.title("Vista de ï¿½rbol en Tkinter")
-#         
-#         self.grid(column=0, row=0, sticky="ns")
-#         self.treeview = ttk.Treeview(self, columns=("inicio","fin", "responsable"))
-# 
-#         self.treeview.pack(expand=True, fill=BOTH)
-#         self.pack()
-#         
-#         
-#         self.treeview.tag_configure("red", background='yellow', foreground="red")
-#         
-# 
-#     def add_tarea(self, tarea):
-#         return self.treeview.insert("", END, text=f'{tarea.codigo} {tarea.descripcion}', values=(tarea.fecha_creacion, tarea.fecha_vencimiento, tarea.estado), tags=("red",))
-# 
-#     def add_subtarea(self, tarea, subtarea):
-#         self.treeview.insert(tarea, END, text=f'{subtarea.codigo}', values=(subtarea.fecha_desde, subtarea.fecha_hasta, subtarea.responsable), tags=("red",))
-# 
-#     
-# df_gant_estado=[]
-# df_gant_responsable=[]
-# df_gant_por_responsable=[]
-
-
+from util.util import date_to_str
 
 class OutputExcelName:
     version=0
@@ -117,6 +76,22 @@ class Planning:
         self.map_excel['Planning']=pd.DataFrame([subtarea for t in self.tareas if t.subtareas for subtarea in TareasPlanning.show_as_lists(t,14)], 
                                          columns=TareaView.headers().split('#'))
 
+    def addPlanningPorResponsable(self):
+        responsable=defaultdict(list)
+
+        dias=14
+        for t in self.tareas:
+            for cod, s in t.subtareas.items():
+                if s.fecha_hasta is not None and s.fecha_hasta <= datetime.datetime.today()+datetime.timedelta(days=dias):
+                    subt="{} ({}-{}..{})".format(t.codigo,cod.value, date_to_str(s.fecha_desde), date_to_str(s.fecha_hasta))
+                    responsable[s.responsable].append(subt)
+        
+        headers=["Responsable","Tareas"]
+        list_by_responsable=[[r, " - ".join(t)] for r,t in responsable.items()]
+        
+        self.map_excel['Planning por responsable']=pd.DataFrame(list_by_responsable, columns=headers)
+
+
     def addValidaciones(self):
         headers=["Razon","Info Razón","Resp. funcional", "Codigo", "Descripcion", "Estado"]
         validaciones=[[razon.what, razon.why, t.responsables.get("RF","") if t.responsables else "", t.codigo, t.descripcion, t.estado]  
@@ -136,9 +111,9 @@ class Planning:
         self.addTodas()
         self.addSinPlanificar()
         self.addPlanning()
+        self.addPlanningPorResponsable()
         self.addValidaciones()
         self.addIncurridos()
-        
     
         self.outFileName=OutputExcelName.getName()
         while True:
